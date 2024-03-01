b013889f0b73751d31bad6ac6fb16e321c14ff2a
rolandbeni <beniroland12@gmail.com>
2024-03-01 10:29:36 +0100

    build: add Lombok support to the project in pom.xml

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -1,7 +1,8 @@
 package nationalize;
 
 import com.google.gson.annotations.SerializedName;
-
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import java.util.List;
 
 public class Nationality {
@@ -46,10 +47,14 @@ public class Nationality {
 
         private String countryId;
         private float probability;
-
+        @Override
+        public String toString() {
+            return ReflectionToStringBuilder.toString(this, ToStringStyle.NO_CLASS_NAME_STYLE);
+        }
         public Country(String countryId, float probability) {
             this.countryId = countryId;
             this.probability = probability;
+
         }
 
         public String getCountryId() {
